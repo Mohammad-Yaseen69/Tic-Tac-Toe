b053aa13dd459187df2f17871d929b2d4864fa7e
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2023-12-12 00:27:21 +0500

    Creating Click functionality for bot

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const playArea = document.querySelector('.play-area');
 const resultSection = document.querySelector('.result');
 const selectionButtonX = document.querySelector('.selection-button-x')
 const selectionButtonO = document.querySelector('.selection-button-o')
-const spanElements = document.querySelectorAll('section span')
 const slider = document.querySelector('.slider');
 const playerX = document.querySelector('.playerX')
 const playerO = document.querySelector('.playerO')
+const spanElements = document.querySelectorAll('section span')
 
-
+let playerSign = 'X'
 function hidingAndShow(hide, show) {
     hide.classList.add('hide')
     hide.classList.remove('show')
@@ -42,10 +42,9 @@ window.onload = () => {
     }
 }
 
-
-
 let cross = '<i class="fa-solid fa-xmark"></i>'
 let circle = '<i class="fa-regular fa-circle"></i>'
+
 function clickedElem(element) {
     if (playerO.classList.contains('player')) {
         element.innerHTML = circle
@@ -57,4 +56,31 @@ function clickedElem(element) {
         OTurnAnimation()
     }
     element.style.pointerEvents = 'none'
+    botClick()
+}
+
+
+function botClick() {
+    const array = []
+    for (let i = 0; i < spanElements.length; i++) {
+        const element = spanElements[i];
+        if (element.childElementCount == 0) {
+            array.push(i)
+        }
+    }
+    setTimeout(() => {
+        let randomClick = array[Math.floor(Math.random() * array.length)]
+        if (array.length > 0) {
+            if (playerO.classList.contains('player')) {
+                spanElements[randomClick].innerHTML = circle
+                XTurnAnimation()
+                playerO.classList.remove('player')
+            }
+            else {
+                spanElements[randomClick].innerHTML = cross
+                OTurnAnimation()
+            }
+            spanElements[randomClick].style.pointerEvents = 'none'
+        }
+    }, 500)
 }
