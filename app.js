4734e2d20f8e067419a828475b5dc69e4854236c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2023-12-13 00:51:09 +0500

    Adding comments in js

--- app.js
+++ app.js
@@ -12,22 +12,30 @@ const winner = document.querySelector('.winner')
 let playerSign = 'X'
 let runBot = true;
 
+// Creating Function for showing and hiding elements
+
 function hidingAndShow(hide, show) {
     hide.classList.add('hide')
     hide.classList.remove('show')
     show.classList.add('show')
     show.classList.remove('hide')
 }
+
+// Creating Function for animaiton when turn change to O
 function OTurnAnimation() {
     slider.style.left = '50%'
     playerX.classList.remove('active')
     playerO.classList.add('active', 'player')
 }
+
+// Creating Function for animation when turn change to X
 function XTurnAnimation() {
     slider.style.left = '2%'
     playerO.classList.remove('active')
     playerX.classList.add('active')
 }
+
+// Adding function for both selection Buttons and adding clickedElem function to the span elements 
 
 window.onload = () => {
     for (let i = 0; i < spanElements.length; i++) {
@@ -53,18 +61,19 @@ window.onload = () => {
     }
 }
 
+// Function for user click
 
 let cross = '<i class="fa-solid fa-xmark"></i>'
 let circle = '<i class="fa-regular fa-circle"></i>'
 
 function clickedElem(element) {
-    if (playerO.classList.contains('player')) {
+    if (playerO.classList.contains('player')) { // if the turn is 0
         element.innerHTML = circle;
         playerSign = 'O';
         element.setAttribute('id', playerSign);
         playerO.classList.remove('player');
         XTurnAnimation();
-    } else {
+    } else {                                  //if the turn is X
         element.innerHTML = cross;
         playerSign = 'X'
         element.setAttribute('id', playerSign);
@@ -75,11 +84,11 @@ function clickedElem(element) {
     showingResult()
 }
 
-
+// Function for bot click
 
 function botClick(runBot) {
     if (runBot) {
-        const array = [];
+        const array = []; // all the unclick / unselected spam elemenets to the array
         for (let i = 0; i < spanElements.length; i++) {
             const element = spanElements[i];
             if (element.childElementCount == 0) {
@@ -87,7 +96,7 @@ function botClick(runBot) {
             }
         }
         setTimeout(() => {
-            let randomClick = array[Math.floor(Math.random() * array.length)];
+            let randomClick = array[Math.floor(Math.random() * array.length)]; // making a random index for bot click
             if (array.length > 0) {
                 if (playerO.classList.contains('player')) {
                     spanElements[randomClick].innerHTML = circle;
@@ -109,17 +118,19 @@ function botClick(runBot) {
     }
 }
 
+// Returning the ids of elements
 
 function returningIds(className) {
     return document.querySelector('.box' + className).id
 }
 
+// Function for checking Winner
 
 function checkingWinner() {
     const winningCombinations = [
-        [1, 2, 3], [4, 5, 6], [7, 8, 9],
-        [1, 4, 7], [2, 5, 8], [3, 6, 9],
-        [1, 5, 9], [3, 5, 7]
+        [1, 2, 3], [4, 5, 6], [7, 8, 9], // Row
+        [1, 4, 7], [2, 5, 8], [3, 6, 9], //Column
+        [1, 5, 9], [3, 5, 7] //Diagonal
     ]
 
     for (const combination of winningCombinations) {
@@ -137,6 +148,7 @@ function checkingWinner() {
     return false;
 }
 
+// Function for checking Draw
 
 function checkingDraw() {
     if (!checkingWinner()) {
@@ -151,6 +163,8 @@ function checkingDraw() {
     }
     return false;
 }
+
+// Showing result
 
 function showingResult() {
     const drawElem = document.querySelector('.winner-h1')
